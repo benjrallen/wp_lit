4cde2781b64723eaffb620ad68a2c7f5280e29d3
Benjamin Allen <benjrallen@gmail.com>
2012-03-09 20:54:37 -0600

    google response handler coming in

--- wp-content/themes/lit/logic/notification.php
+++ wp-content/themes/lit/logic/notification.php
@@ -11,10 +11,11 @@
   if( isset($_REQUEST['serial-number'])){
     error_log('i have a serial number and it is: '.$_REQUEST['serial-number']);
     
+    require_once('lib/googleResponseHandler.php');
     
-    header('HTTP/1.1 200 OK');
-    print 'serial-number='.$_REQUEST['serial-number'];
-    die;
+    // header('HTTP/1.1 200 OK');
+    // $acknowledgement = 'serial-number='.$_REQUEST['serial-number'];
+    // die( $acknowledgement );
   }
   
   
