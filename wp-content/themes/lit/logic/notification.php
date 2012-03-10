c2ae47f54cfecc12d4822ae73acbbc372baf0384
Benjamin Allen <benjrallen@gmail.com>
2012-03-09 20:08:25 -0600

    serial number

--- wp-content/themes/lit/logic/notification.php
+++ wp-content/themes/lit/logic/notification.php
@@ -5,6 +5,12 @@
   //log out the request
   $request = $_REQUEST;
   
-  error_log( print_r($request, true) );
+  error_log( print_r($_REQUEST, true) );
+  
+  
+  if( isset($_REQUEST['serial-number'])){
+    error_log('i have a serial number and it is: '.$_REQUEST['serial-number']);
+  }
+  
   
 die;
