1f98221dff2aa4b12ee17623ff30701f85b0bf16
Benjamin Allen <benjrallen@gmail.com>
2012-03-10 21:02:56 -0600

    setting up email... complete?

--- wp-content/themes/lit/logic/notification.php
+++ wp-content/themes/lit/logic/notification.php
@@ -7,68 +7,6 @@
   
   //error_log( print_r($_REQUEST, true) );
 
-  //used in both paths
-  function lit_build_order_text( $order ){
-    $fields = array(
-      'Order Status' =>     'status',
-      'Salutation' =>       'salutation',
-      'First Name' =>       'firstname',
-      'Last Name' =>        'lastname',
-      'Address' =>          'address',
-      'City' =>             'city',
-      'State' =>            'state',
-      'Zip' =>              'zip',
-      'Country' =>          'country',
-      'Email' =>            'email',
-      'Phone' =>            'phone',
-      'Order Total' =>      'order_total',
-      'Payment Gateway' =>  'gateway_used',
-      'Order ID' =>         'orderid'
-    );
-    
-    $message = '';
-    
-    if( !empty($order) ){
-      foreach( $fields as $nice => $f ){
-          $message .= $nice . ':    '.$order[$f] . "\r\n";
-      }
-    }
-    
-    $message .= "\r\n";
-    
-    return $message;
-  }
-
-  //send email function used in both pathways
-  function  send_lit_order_email( $subject, $message ){
-    
-    $toArray =  ( GW_SANDBOX ? 
-                  array( 'benjrallen@gmail.com') :
-                  array( 'benjrallen@gmail.com', 'nakedincorners@gmail.com')
-                );
-    
-    $from = ( GW_SANDBOX ? 'no-reply@dev.benjrallen.com' : 'info@litmotors.com' );
-    
-    $fromName = ( GW_SANDBOX ? 'Dev Server' : 'Lit Orders');
-    
-    $headers = 'From: '.$fromName.'<'. $from . '>' . "\r\n" .
-        'Reply-To: '.$fromName.'Lit Orders<'. $from . '>' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();    
-    
-    $to = '';
-    
-    for( $i = 0, $j = count($toArray); $i < $j; $i++ ){
-      $to .= $i == 0 ? '' : ',';
-      $to .= $toArray[$i];
-    }
-    
-    //error_log( $to );
-    //error_log( $headers );
-    
-    mail( $to, $subject, $message, $headers );
-    
-  }
-
   /*
   if ( $_REQUEST['send-test'] == 'email' ){
     
