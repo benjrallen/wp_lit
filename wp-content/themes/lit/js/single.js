26de3b40046666c84fdce9fbcc9057b1b100019c
Benjamin Allen <benjrallen@gmail.com>
2012-05-10 11:59:41 -0500

    single video analytics

--- wp-content/themes/lit/js/single.js
+++ wp-content/themes/lit/js/single.js
@@ -1,3 +1,16 @@
+/*
+ * Try/Catch the console
+ */
+try{
+    console.log('Hello Console.');
+} catch(e) {
+    window.console = {};
+    var cMethods = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(",");
+    for (var i=0; i<cMethods.length; i++) {
+        console[ cMethods[i] ] = function(){};
+    }
+}
+
 (function($){
 	//specifically written to use for jwplayer videos
 	function EaseVideo( $el ){
