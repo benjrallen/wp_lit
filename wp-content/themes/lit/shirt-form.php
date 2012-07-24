3c1b189a4874c470423d0e6f7e8a7c7542fb5ce9
Website Administrator <website@litmotors.com>
2012-07-23 22:19:20 -0500

    preparing to fix t-shirt mess up

--- wp-content/themes/lit/shirt-form.php
+++ wp-content/themes/lit/shirt-form.php
@@ -30,17 +30,32 @@
 
   <button id="formSubmit" type="submit"><span class="ico"></span><span>Buy Now</span></button>
 
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="H4RFZ69HK7VJN">
+<table>
+<tr><td><input type="hidden" name="on0" value="Shipping">Shipping</td></tr><tr><td><select name="os0">
+	<option value="North America">North America$30.00 USD</option>
+	<option value="All Others">All Others$40.00 USD</option>
+</select> </td></tr>
+<tr><td><input type="hidden" name="on1" value="Size">Size</td></tr><tr><td><select name="os1">
+	<option value="Small">Small</option>
+	<option value="Medium">Medium</option>
+	<option value="Large">Large</option>
+	<option value="Extra Large">Extra Large</option>
+</select> </td></tr>
+<tr><td><input type="hidden" name="on2" value="Color">Color</td></tr><tr><td><select name="os2">
+	<option value="Black">Black</option>
+	<option value="Light Gray">Light Gray</option>
+	<option value="Dark Gray">Dark Gray</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="USD">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 
 
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" id="paypalForm">
-	<input type="hidden" name="on0" value="Color">
-	<input type="hidden" name="os0" value="Black">
-	
-	<input type="hidden" name="on1" value="Size">
-	<input type="hidden" name="os1" value="Small">
-	<input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="H4RFZ69HK7VJN">
-</form>
 
 <form action="https://checkout.google.com/api/checkout/v2/checkoutForm/Merchant/704111282563175" id="googleForm" method="post" name="BB_BuyButtonForm" target="_top">
     <table cellpadding="5" cellspacing="0" width="1%">
