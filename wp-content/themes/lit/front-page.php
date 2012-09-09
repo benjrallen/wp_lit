64a1bfdbc7a333e592615d0d7297a716c6fcf951
Benjamin Allen <benjrallen@gmail.com>
2012-09-09 14:06:26 -0700

    test hello

--- wp-content/themes/lit/front-page.php
+++ wp-content/themes/lit/front-page.php
@@ -14,7 +14,7 @@
  */
 
 get_header(); ?>
-
+oh hello!
 			<?php
 			/* Run the loop to output the posts.
 			 * If you want to overload this in a child theme then include a file
@@ -99,122 +99,6 @@ get_header(); ?>
     </video>
     <!--<![endif]-->
 
-<?php /* ?>
-<!--[if lte IE 9 ]>
-		<object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' width='720' height='480' id='single1' name='single1'>
-      <param name="movie" value="<?php bloginfo('template_directory'); ?>/turntable/player.swf" />
-			<param name='allowfullscreen' value='true'>
-			<param name='allowscriptaccess' value='always'>
-			<param name='wmode' value='transparent'>
-			<param name='flashvars' value='file=<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.mp4&image=<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.jpg&controls=true&dock=false&autostart=false&quality=false&repeat=none'>
-			<embed
-				type='application/x-shockwave-flash'
-				id='single2'
-				name='single2'
-				src="<?php bloginfo('template_directory'); ?>/turntable/player.swf"
-				width='864'
-				height='480'
-				bgcolor='undefined'
-				allowscriptaccess='always'
-				allowfullscreen='true'
-				wmode='transparent'
-        flashvars="controls=true&image=<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.jpg&file=<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.mp4" 
-			/>
-    		<img alt="Lit Motors" src="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.jpg" width="864" height="480" title="Sorry, No video playback capabilities." />
-		</object>
-<![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!-->
-<video id="splash" width="864" height="480" class="mejs-myskin" controls="controls" preload="auto" poster="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.jpg" >
-   <source src="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.mp4" type="video/mp4"></source>
-   <source src="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.webm" type="video/webm"></source>
-   <source src="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.ogv" type="video/ogg"></source>
-
-		<object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' width='864' height='480' id='single1' name='single1'>
-     <param name="movie" value="<?php bloginfo('template_directory'); ?>/turntable/player.swf" />
-			<param name='allowfullscreen' value='true'>
-			<param name='allowscriptaccess' value='always'>
-			<param name='wmode' value='transparent'>
-			<param name='flashvars' value='file=<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.mp4&image=<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.jpg&controls=true&dock=false&autostart=false&quality=false&repeat=none'>
-			<embed
-				type='application/x-shockwave-flash'
-				id='single2'
-				name='single2'
-				src="<?php bloginfo('template_directory'); ?>/turntable/player.swf"
-				width='864'
-				height='480'
-				bgcolor='undefined'
-				allowscriptaccess='always'
-				allowfullscreen='true'
-				wmode='transparent'
-       flashvars="controls=true&image=<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.jpg&file=<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.mp4" 
-			/>
-   		<img alt="Lit Motors" src="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.jpg" width="864" height="480" title="Sorry, No video playback capabilities." />
-		</object>
-</video>
-<!--<![endif]-->
-<?php */ ?>
-
-<?php /* ?>
-    <video id="splash" width="720" height="480" class="mejs-myskin" autoplay="autoplay" controls="controls" preload="auto" poster="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.jpg" >
-       <source src="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.mp4" type="video/mp4"></source>
-       <source src="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.webm" type="video/webm"></source>
-       <source src="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.ogv" type="video/ogg"></source>
- 			<object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' width='720' height='480' id='single1' name='single1'>
-        <param name="movie" value="<?php bloginfo('template_directory'); ?>/turntable/player.swf" />
- 				<param name='allowfullscreen' value='true'>
- 				<param name='allowscriptaccess' value='always'>
- 				<param name='wmode' value='transparent'>
-				<param name='flashvars' value='file=<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.mp4&controls=true&dock=false&autostart=true&quality=false&repeat=none'>
- 				<embed
- 					type='application/x-shockwave-flash'
- 					id='single2'
- 					name='single2'
- 					src="<?php bloginfo('template_directory'); ?>/turntable/player.swf"
- 					width='720'
- 					height='480'
- 					bgcolor='undefined'
- 					allowscriptaccess='always'
- 					allowfullscreen='true'
- 					wmode='transparent'
-          flashvars="controls=true&file=<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.mp4" 
- 				/>
-      		<img alt="Lit Motors" src="<?php bloginfo('template_directory'); ?>/streetdrive/streetdrive.jpg" width="640" height="360" title="Sorry, No video playback capabilities." />
- 			</object>
-     </video>
-<?php */ ?>
-
-<?php /* ?>
-		<source src="<?php echo site_url("/vid/2_turntable-web.mp4"); ?>" type="video/mp4"  /> 
-		<source src="<?php echo site_url("/vid/2_turntable-web.webm"); ?>" type="video/webm" /> 
-		<source src="<?php echo site_url("/vid/2_turntable-web.ogv"); ?>" type="video/ogv" /> 
-
-
-   <video id="splash" width="960" height="500" loop="loop" autoplay="autoplay">
-      <source src="<?php bloginfo('template_directory'); ?>/turntable/2_turntable-web.mp4" type="video/mp4"></source>
-      <source src="<?php bloginfo('template_directory'); ?>/turntable/2_turntable-web.webm" type="video/webm"></source>
-      <source src="<?php bloginfo('template_directory'); ?>/turntable/2_turntable-web.ogv" type="video/ogg"></source>
-			<object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' width='960' height='500' id='single1' name='single1'>
-				<param name='movie' value="<?php bloginfo('template_directory'); ?>/turntable/player.swf">
-				<param name='allowfullscreen' value='true'>
-				<param name='allowscriptaccess' value='always'>
-				<param name='wmode' value='transparent'>
-				<param name='flashvars' value='file=<?php bloginfo('template_directory'); ?>/turntable/2_turntable-web.mp4&controlbar=none&dock=false&autostart=true&icons=false&quality=false&repeat=always'>
-				<embed
-					type='application/x-shockwave-flash'
-					id='single2'
-					name='single2'
-					src="<?php bloginfo('template_directory'); ?>/turntable/player.swf"
-					width='960'
-					height='500'
-					bgcolor='undefined'
-					allowscriptaccess='always'
-					allowfullscreen='true'
-					wmode='transparent'
-					flashvars="file=<?php bloginfo('template_directory'); ?>/turntable/2_turntable-web.mp4&controlbar=none&dock=false&autostart=true&icons=false&quality=false&repeat=always"
-				/>
-			</object>
-    </video>
-<?php */ ?>
   </div>
   <div class="clearfix"></div>
 
